9d8135fbab36dd67583ad6177e5a2ca2dd8576f1
Madeline Hough <houghm2@rpi.edu>
2025-05-30 13:20:18 -0400

    Maddie made a comment

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main
 {
 	public static void main(String[] args) {
-		System.out.println("i'm bad at psoft");
+		System.out.println("i'm bad at psoft"); // Maddie made a change here
 	}
 }
